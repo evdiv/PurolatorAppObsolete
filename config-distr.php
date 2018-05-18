66dd905da43f4e7a4347b8e2551c753027bf3931
Eugene Dvoir <vakuka@gmail.com>
2018-05-18 10:57:04 -0400

    Update configuration options

--- config-distr.php
+++ config-distr.php
@@ -23,6 +23,7 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 
+
 //************************************
 //Purolator Credentials
 
@@ -32,23 +33,36 @@ define("APP_PUROLATOR_BILLING_ACCOUNT", "");
 define("APP_PUROLATOR_FREIGHT_ACCOUNT", "");
 
 
+
+//*************************************
+//Purolator Production URLs
+/*
 define("APP_PUROLATOR_ESTIMATING_URL", "https://webservices.purolator.com/PWS/V1/Estimating/EstimatingService.asmx");
 define("APP_PUROLATOR_SHIPMENT_URL", "https://webservices.purolator.com/PWS/V1/Shipping/ShippingService.asmx");
 define("APP_PUROLATOR_SHIPMENT_DOCUMENTS_URL", "https://webservices.purolator.com/PWS/V1/ShippingDocuments/ShippingDocumentsService.asmx");
 define("APP_PUROLATOR_RETURN_SHIPMENT_URL", "https://webservices.purolator.com/EWS/V2/ReturnsManagement/ReturnsManagementService.asmx");
+*/
+
+
+//*************************************
+//Purolator Development URLs
+
+define("APP_PUROLATOR_ESTIMATING_URL", "https://devwebservices.purolator.com/PWS/V1/Estimating/EstimatingService.asmx");
+define("APP_PUROLATOR_SHIPMENT_URL", "https://devwebservices.purolator.com/PWS/V1/Shipping/ShippingService.asmx");
+define("APP_PUROLATOR_SHIPMENT_DOCUMENTS_URL", "https://devwebservices.purolator.com/PWS/V1/ShippingDocuments/ShippingDocumentsService.asmx");
+define("APP_PUROLATOR_RETURN_SHIPMENT_URL", "https://devwebservices.purolator.com/EWS/V2/ReturnsManagement/ReturnsManagementService.asmx");
 
 
 
 //****************************************
-// Require helper functions
+// Autoloader and helper functions
 
 require_once "./helpers.php"; 
-
 
 
 //******************************************
 // Incoming Parameters
 
-$ordersID = ForceIncomingInt('OrdersID', 0);
-$returnShipment = ForceIncomingInt('ReturnShipment', 0);
+$ordersID = getIncomingInt('OrdersID');
+$returnShipment = getIncomingInt('ReturnShipment');
 
